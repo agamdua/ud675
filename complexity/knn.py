e509cdf2fe0679278a2b7f678703d8411a81f436
Agam Dua <agam@comfylabs.io>
2015-01-03 19:55:21 -0500

    Added: second plot to analyze the bias/variance + total predication error

--- complexity/knn.py
+++ complexity/knn.py
@@ -23,22 +23,33 @@ train_err = zeros(len(k_range))
 test_err = zeros(len(k_range))
 
 for i, k in enumerate(k_range):
-	# Set up a KNN model that regressors over k neighbors
+    # Set up a KNN model that regressors over k neighbors
     neigh = KNeighborsRegressor(n_neighbors=k)
-    
+
     # Fit the learner to the training data
     neigh.fit(X_train, y_train)
 
-	# Find the MSE on the training set
+    # Find the MSE on the training set
     train_err[i] = mean_squared_error(y_train, neigh.predict(X_train))
     # Find the MSE on the testing set
     test_err[i] = mean_squared_error(y_test, neigh.predict(X_test))
 
 # Plot training and test error as a function of k
-pl.figure()
+pl.figure(1)
 pl.title('kNN: Error as a function of k')
 pl.plot(k_range, test_err, lw=2, label = 'test error')
 pl.plot(k_range, train_err, lw=2, label = 'training error')
+pl.legend()
+pl.xlabel('k')
+pl.ylabel('MS error')
+# pl.show()
+
+import numpy
+pl.figure(2)
+pl.title('Bias-variance tradeoff')
+pl.plot(k_range, numpy.square(test_err), lw=2, label = 'bias squared')
+pl.plot(k_range, train_err, lw=2, label = 'variance')
+pl.plot(k_range, numpy.square(test_err) + train_err, lw=2, label = 'prediction error')
 pl.legend()
 pl.xlabel('k')
 pl.ylabel('MS error')
